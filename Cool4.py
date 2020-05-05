f104cdf16d4ed4f95c2ca25bb59233e14b84f5f6
erhuht <erhuht15@gmail.com>
2020-05-05 21:55:56 +0300

    Added biome structure too Cool4.py

--- Cool4.py
+++ Cool4.py
@@ -14,8 +14,18 @@ weights = [5, 1, 5]
 printing = [" "*WIDTH*2]*HEIGHT
 
 
+class Biome:
+    stoppable = True
+    @staticmethod
+    def generate(previous, number):
+        return previous
 
-class Mountains:
+    @staticmethod
+    def output(printing, number):
+        printing[number] = printing[number][:-1] + "#"
+
+
+class Mountains(Biome):
     @staticmethod
     def generate(previous, number):
         if number < HEIGHT-1 and number > 0:
@@ -46,36 +56,67 @@ class Mountains:
         
         return printing
 
-class Grasslands:
-    @staticmethod
-    def generate(previous, number):
-        if number < HEIGHT-1 and number > 0:
-            previous = random.choices([-1, 0, 1], [1, 2, 1])[0]
-        elif number >= HEIGHT-1:
-            previous = random.choices([-1, 0], [1, 2])[0]
+class Grasslands(Biome):
+    tree_bool = False
+    tree_index = 0
+    tree = ["   ||   ",
+            "   ||   ",
+            "########",
+            " ###### ",
+            "   ##   "]
+
+    @classmethod
+    def generate(cls, previous, number):
+        if not cls.tree_bool:
+            if number < HEIGHT-1 and number > 0:
+                previous = random.choices([-1, 0, 1], [1, 25, 1])[0]
+            elif number >= HEIGHT-1:
+                previous = random.choices([-1, 0], [1, 25])[0]
+            else:
+                previous = random.choices([0, 1], [25, 1])[0]
         else:
-            previous = random.choices([0, 1], [2, 1])[0]
+            previous = 0
         return previous
 
-    @staticmethod
-    def output(printing, number):
+    @classmethod
+    def output(cls, printing, number):
         #Adds a # to the line corresponding to the number
         printing[number] = printing[number][:-1] + "#"
+        if number > 0:
+            printing[number-1] = printing[number-1][:-1] + random.choices([" ", ",", ".", "v"], [20, 2, 1, 5])[0]
+        
+        #Tree
+        if  random.random() > 0.98 and number > 3:
+            cls.stoppable = False
+            cls.tree_bool = True
+        
+        if cls.tree_bool:
+            for i in range(len(cls.tree)):
+                printing[number-(1+i)] = printing[number-(1+i)][:-1] + cls.tree[i][cls.tree_index]
+            cls.tree_index += 1
+        
+        if cls.tree_index >= len(cls.tree[0]):
+            cls.stoppable = True
+            cls.tree_bool = False
+            cls.tree_index = 0
 
         return printing
 
-biome = Mountains
+#possible future biome: https://ascii.co.uk/art/farms
+
+biome = Grasslands
 lowland_biomes = [Mountains, Grasslands]
 anywhere_biomes = [Mountains]
 
 while True:
     try:
         print("\n"*(HEIGHT*2)) # Flushes the text
-        if random.random() > 0.99:
-            if number > HEIGHT//2:
-                biome = random.choice(lowland_biomes)
-            else:
-                biome = random.choice(anywhere_biomes)
+        if random.random() > 0.995:
+            if biome.stoppable:
+                if number > HEIGHT//2:
+                    biome = random.choice(lowland_biomes)
+                else:
+                    biome = random.choice(anywhere_biomes)
         
 
         #Random generates where the new symbol should be. The weights make it go longer in the same direction 
@@ -84,8 +125,7 @@ while True:
         
         #Moves every line to the left
         for s in range(len(printing)):
-            printing[s] = printing[s][1:]
-            printing[s] += " "
+            printing[s] = printing[s][1:] + " "
         
         #Adds a # to the line corresponding to the number
         printing = biome.output(printing, number)
